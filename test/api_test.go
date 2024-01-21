74026f947017eaa0048c6fee043a4fe7f055b99a
kmigielek <kazimierzmigielek@gmail.com>
2024-01-21 16:43:42 +0100

    broken

--- test/api_test.go
+++ test/api_test.go
@@ -2,10 +2,13 @@ package test
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/caarlos0/env/v10"
+	"github.com/jaswdr/faker"
 	advancedbilling "github.com/maxio-com/ab-golang-sdk"
 	"github.com/maxio-com/ab-golang-sdk/models"
 	"github.com/stretchr/testify/suite"
@@ -20,12 +23,15 @@ type Config struct {
 
 type APITestSuite struct {
 	suite.Suite
+	fkr faker.Faker
 
 	client             advancedbilling.ClientInterface
 	unauthorizedClient advancedbilling.ClientInterface
 }
 
 func (s *APITestSuite) SetupTest() {
+	s.fkr = faker.New()
+
 	cfg := Config{}
 	if err := env.Parse(&cfg); err != nil {
 		panic(err)
@@ -47,89 +53,155 @@ func (s *APITestSuite) SetupTest() {
 	s.unauthorizedClient = advancedbilling.NewClient(configUnauthorized)
 }
 
-func (s *APITestSuite) TestExample() {
-	s.T().Log(s.client.Configuration().Domain())
-}
-
-func (s *APITestSuite) TestReadSite() {
-	cases := []struct {
-		name        string
-		client      advancedbilling.ClientInterface
-		assert      func(*testing.T, models.ApiResponse[models.SiteResponse])
-		expectedErr bool
-	}{
-		{
-			name:        "success",
-			client:      s.client,
-			expectedErr: false,
-			assert: func(t *testing.T, resp models.ApiResponse[models.SiteResponse]) {
-				s.Equal(http.StatusOK, resp.Response.StatusCode, "status code")
-
-				respSite := resp.Data.Site
-				s.Equal(4718, *respSite.Id, "ID")
-				s.Equal("GO SDK env", *respSite.Name, "Name")
-				s.Equal("go-sdk", *respSite.Subdomain, "Subdomain")
-				s.Equal("USD", *respSite.Currency, "Currency")
-				s.Equal(722159, *respSite.SellerId, "SellerID")
-				s.EqualValues([]string{}, respSite.NonPrimaryCurrencies, "NonPrimaryCurrencies")
-				s.True(*respSite.RelationshipInvoicingEnabled, "RelationshipInvoiceEnabled")
-				s.False(*respSite.CustomerHierarchyEnabled, "CustomerHierarchyEnabled,")
-				s.False(*respSite.WhopaysEnabled, "WhopaysEnabled")
-				s.Equal("self-ungrouped", *respSite.WhopaysDefaultPayer, "WhopaysDefaultPayer")
-				s.Equal(string(models.PaymentCollectionMethod_AUTOMATIC), *respSite.DefaultPaymentCollectionMethod, "DefaultPaymentCollectionMethod")
-
-				allocationSettings := respSite.AllocationSettings
-				s.Equal(models.CreditType_PRORATED, *allocationSettings.UpgradeCharge.Value(), "UpgradeCharge")
-				s.Equal(models.CreditType_NONE, *allocationSettings.DowngradeCredit.Value(), "DowngradeCredit")
-				s.Equal("true", *allocationSettings.AccrueCharge, "AccrueCharge")
-
-				organizationAddress := respSite.OrganizationAddress
-				s.Equal("Asdf Street", *organizationAddress.Street.Value(), "Street")
-				s.Equal("123/444", *organizationAddress.Line2.Value(), "Line2")
-				s.Equal("San Antonio", *organizationAddress.City.Value(), "City")
-				s.Equal("TX", *organizationAddress.State.Value(), "State")
-				s.Equal("78015", *organizationAddress.Zip.Value(), "Zip")
-				s.Equal("US", *organizationAddress.Country.Value(), "Country")
-				s.Equal("Developer Experience", *organizationAddress.Name.Value(), "AddressName")
-				s.Equal("555 111 222", *organizationAddress.Phone.Value(), "Phone")
-
-				taxConfiguration := respSite.TaxConfiguration
-				s.Equal(models.TaxConfigurationKind_CUSTOM, *taxConfiguration.Kind, "Kind")
-				s.Equal(models.TaxDestinationAddress_SHIPPINGTHENBILLING, *taxConfiguration.DestinationAddress, "TaxDestinationAddress")
-				s.False(*taxConfiguration.FullyConfigured, "FullyConfigured")
-
-				netTerms := respSite.NetTerms
-				s.Equal(0, *netTerms.DefaultNetTerms, "DefaultNetTerms")
-				s.Equal(0, *netTerms.AutomaticNetTerms, "AutomaticNetTerms")
-				s.Equal(0, *netTerms.RemittanceNetTerms, "RemittanceNetTerms")
-				s.False(*netTerms.NetTermsOnRemittanceSignupsEnabled, "NetTermsOnRemittanceSignupsEnabled")
-				s.False(*netTerms.CustomNetTermsEnabled, "CustomNetTermsEnabled")
-
-				s.True(*respSite.Test, "Test")
-			},
-		},
-		{
-			name:   "unauthorized",
-			client: s.unauthorizedClient,
-			assert: func(t *testing.T, resp models.ApiResponse[models.SiteResponse]) {
-				s.Equal(http.StatusUnauthorized, resp.Response.StatusCode)
-			},
-			expectedErr: true,
-		},
-	}
-
-	for _, c := range cases {
-		s.T().Run(c.name, func(t *testing.T) {
-			resp, err := c.client.SitesController().ReadSite(context.Background())
-			if !c.expectedErr {
-				s.NoErrorf(err, "could not read site")
-			}
-			c.assert(t, resp)
-		})
-	}
-
-}
-
 func TestAPITestSuite(t *testing.T) {
 	suite.Run(t, new(APITestSuite))
+}
+
+func (s *APITestSuite) createCustomer(ctx context.Context) models.Customer {
+	person := s.fkr.Person()
+
+	resp, err := s.client.CustomersController().CreateCustomer(ctx, &models.CreateCustomerRequest{
+		Customer: models.CreateCustomer{
+			FirstName:    person.FirstName(),
+			LastName:     person.LastName(),
+			Email:        person.Contact().Email,
+			Organization: strPtr(s.fkr.Company().Name()),
+			Reference:    strPtr(fmt.Sprintf("%d", s.fkr.RandomNumber(10))),
+			Address:      strPtr(person.Faker.Address().StreetAddress()),
+			City:         strPtr(person.Faker.Address().City()),
+			State:        strPtr(person.Faker.Address().State()),
+			Zip:          strPtr(person.Faker.Address().PostCode()),
+			Country:      strPtr(person.Faker.Address().Country()),
+			Phone:        strPtr(person.Faker.Address().Country()),
+		},
+	})
+
+	s.NoErrorf(err, "create customer err")
+	s.Equalf(http.StatusCreated, resp.Response.StatusCode, "create customer status code")
+
+	return resp.Data.Customer
+}
+
+func (s *APITestSuite) createProductFamily(ctx context.Context) models.ProductFamily {
+	resp, err := s.client.ProductFamiliesController().CreateProductFamily(ctx, &models.CreateProductFamilyRequest{
+		ProductFamily: models.CreateProductFamily{
+			Name: strPtr(s.fkr.Company().Name()),
+		},
+	})
+
+	s.NoErrorf(err, "create product family err")
+	s.Equalf(http.StatusCreated, resp.Response.StatusCode, "create product family status code")
+
+	return *resp.Data.ProductFamily
+}
+
+func (s *APITestSuite) createProduct(ctx context.Context, productFamilyID int) models.Product {
+	resp, err := s.client.ProductsController().CreateProduct(ctx, productFamilyID, &models.CreateOrUpdateProductRequest{
+		Product: models.CreateOrUpdateProduct{
+			Name:         "Test",
+			Description:  "Testable product",
+			PriceInCents: 50,
+			Interval:     1,
+			IntervalUnit: models.IntervalUnit_MONTH,
+		},
+	})
+
+	s.NoErrorf(err, "create product err")
+	s.Equalf(http.StatusCreated, resp.Response.StatusCode, "create product status code")
+
+	return resp.Data.Product
+}
+
+func (s *APITestSuite) createCoupon(ctx context.Context, productFamilyID int) models.Coupon {
+	coupon := &models.Coupon{
+		Name:                        strPtr("100\\% off first month of usage"),
+		Code:                        strPtr("100OFF"),
+		Description:                 strPtr("100\\% off one-time"),
+		Percentage:                  models.NewOptional[string](strPtr("100")),
+		AllowNegativeBalance:        boolPtr(false),
+		Recurring:                   boolPtr(false),
+		EndDate:                     models.NewOptional[string](strPtr(newDate())),
+		ProductFamilyId:             &productFamilyID,
+		Stackable:                   boolPtr(false),
+		ExcludeMidPeriodAllocations: boolPtr(true),
+		ApplyOnCancelAtEndOfPeriod:  boolPtr(true),
+	}
+
+	resp, err := s.client.CouponsController().CreateCoupon(ctx, productFamilyID, &models.CreateOrUpdateCoupon{
+		Coupon: interfacePtr(coupon),
+	})
+
+	s.NoErrorf(err, "create coupon err")
+	s.Equalf(http.StatusCreated, resp.Response.StatusCode, "create coupon err")
+
+	return *resp.Data.Coupon
+}
+
+type MeteredComponent struct {
+	Name          string  `json:"name"`
+	UnitName      string  `json:"unit_name"`
+	Taxable       bool    `json:"taxable"`
+	PricingScheme string  `json:"pricing_scheme"`
+	Prices        []Price `json:"prices"`
+}
+
+type Price struct {
+	StartingQuantity int `json:"starting_quantity"`
+	UnitPrice        int `json:"unit_price"`
+}
+
+func (s *APITestSuite) createMeteredComponent(ctx context.Context, productFamilyID int) models.Component {
+	component := struct {
+		MeteredComponent MeteredComponent `json:"metered_component"`
+	}{
+		MeteredComponent: MeteredComponent{
+			Name:          "test 2",
+			UnitName:      "test message",
+			Taxable:       false,
+			PricingScheme: "stairstep",
+			Prices: []Price{{
+				StartingQuantity: 1,
+				UnitPrice:        1,
+			},
+			},
+		},
+	}
+
+	resp, err := s.client.ComponentsController().CreateComponent(
+		ctx,
+		productFamilyID,
+		models.ComponentKindPath_METEREDCOMPONENTS,
+		interfacePtr(component),
+	)
+
+	s.NoErrorf(err, "create component err")
+	s.Equalf(http.StatusCreated, resp.Response.StatusCode, "create component err")
+
+	return resp.Data.Component
+}
+
+func strPtr(v string) *string {
+	return &v
+}
+
+func boolPtr(v bool) *bool {
+	return &v
+}
+
+func intPtr(v int) *int {
+	return &v
+}
+
+func interfacePtr(v interface{}) *interface{} {
+	return &v
+}
+
+func toPtr[T any](v T) *T {
+	return &v
+}
+
+func newDate() string {
+	t := time.Now().Add(time.Hour)
+
+	return fmt.Sprintf("%d-%d-%d", t.Year(), t.Month(), t.Day())
 }
---
+++ test/site_test.go
@@ -0,0 +1,89 @@
+package test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	advancedbilling "github.com/maxio-com/ab-golang-sdk"
+	"github.com/maxio-com/ab-golang-sdk/models"
+)
+
+func (s *APITestSuite) TestReadSite() {
+	cases := []struct {
+		name        string
+		client      advancedbilling.ClientInterface
+		assert      func(*testing.T, models.ApiResponse[models.SiteResponse])
+		expectedErr bool
+	}{
+		{
+			name:        "success",
+			client:      s.client,
+			expectedErr: false,
+			assert: func(t *testing.T, resp models.ApiResponse[models.SiteResponse]) {
+				s.Equal(http.StatusOK, resp.Response.StatusCode, "status code")
+
+				respSite := resp.Data.Site
+				s.Equal(4718, *respSite.Id, "ID")
+				s.Equal("GO SDK env", *respSite.Name, "Name")
+				s.Equal("go-sdk", *respSite.Subdomain, "Subdomain")
+				s.Equal("USD", *respSite.Currency, "Currency")
+				s.Equal(722159, *respSite.SellerId, "SellerID")
+				s.EqualValues([]string{}, respSite.NonPrimaryCurrencies, "NonPrimaryCurrencies")
+				s.True(*respSite.RelationshipInvoicingEnabled, "RelationshipInvoiceEnabled")
+				s.False(*respSite.CustomerHierarchyEnabled, "CustomerHierarchyEnabled,")
+				s.False(*respSite.WhopaysEnabled, "WhopaysEnabled")
+				s.Equal("self-ungrouped", *respSite.WhopaysDefaultPayer, "WhopaysDefaultPayer")
+				s.Equal(string(models.PaymentCollectionMethod_AUTOMATIC), *respSite.DefaultPaymentCollectionMethod, "DefaultPaymentCollectionMethod")
+
+				allocationSettings := respSite.AllocationSettings
+				s.Equal(models.CreditType_PRORATED, *allocationSettings.UpgradeCharge.Value(), "UpgradeCharge")
+				s.Equal(models.CreditType_NONE, *allocationSettings.DowngradeCredit.Value(), "DowngradeCredit")
+				s.Equal("true", *allocationSettings.AccrueCharge, "AccrueCharge")
+
+				organizationAddress := respSite.OrganizationAddress
+				s.Equal("Asdf Street", *organizationAddress.Street.Value(), "Street")
+				s.Equal("123/444", *organizationAddress.Line2.Value(), "Line2")
+				s.Equal("San Antonio", *organizationAddress.City.Value(), "City")
+				s.Equal("TX", *organizationAddress.State.Value(), "State")
+				s.Equal("78015", *organizationAddress.Zip.Value(), "Zip")
+				s.Equal("US", *organizationAddress.Country.Value(), "Country")
+				s.Equal("Developer Experience", *organizationAddress.Name.Value(), "AddressName")
+				s.Equal("555 111 222", *organizationAddress.Phone.Value(), "Phone")
+
+				taxConfiguration := respSite.TaxConfiguration
+				s.Equal(models.TaxConfigurationKind_CUSTOM, *taxConfiguration.Kind, "Kind")
+				s.Equal(models.TaxDestinationAddress_SHIPPINGTHENBILLING, *taxConfiguration.DestinationAddress, "TaxDestinationAddress")
+				s.False(*taxConfiguration.FullyConfigured, "FullyConfigured")
+
+				netTerms := respSite.NetTerms
+				s.Equal(0, *netTerms.DefaultNetTerms, "DefaultNetTerms")
+				s.Equal(0, *netTerms.AutomaticNetTerms, "AutomaticNetTerms")
+				s.Equal(0, *netTerms.RemittanceNetTerms, "RemittanceNetTerms")
+				s.False(*netTerms.NetTermsOnRemittanceSignupsEnabled, "NetTermsOnRemittanceSignupsEnabled")
+				s.False(*netTerms.CustomNetTermsEnabled, "CustomNetTermsEnabled")
+
+				s.True(*respSite.Test, "Test")
+			},
+		},
+		{
+			name:   "unauthorized",
+			client: s.unauthorizedClient,
+			assert: func(t *testing.T, resp models.ApiResponse[models.SiteResponse]) {
+				s.Equal(http.StatusUnauthorized, resp.Response.StatusCode)
+			},
+			expectedErr: true,
+		},
+	}
+
+	for _, c := range cases {
+		s.T().Run(c.name, func(t *testing.T) {
+			resp, err := c.client.SitesController().ReadSite(context.Background())
+			if !c.expectedErr {
+				s.NoErrorf(err, "could not read site")
+			}
+			c.assert(t, resp)
+		})
+	}
+
+}
